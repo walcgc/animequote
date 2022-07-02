ad60bc0ff50534df15525666ea398fca167f62ef
William Chan <william.chan@gcash.com>
2022-07-02 12:59:42 +0800

    db working with recyclerview

--- app/src/main/java/com/random/animequote/adapter/QuotesAdapter.kt
+++ app/src/main/java/com/random/animequote/adapter/QuotesAdapter.kt
@@ -1,39 +1,57 @@
 package com.random.animequote.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.random.animequote.R
+import com.random.animequote.db.DBHelper
+import com.random.animequote.model.Quote
 
-class QuotesAdapter : RecyclerView.Adapter<QuotesAdapter.ViewHolder>() {
+class QuotesAdapter(
+    context: Context,
+    anime: ArrayList<String>,
+    character: ArrayList<String>,
+    quote: ArrayList<String>,
+    imgurl: ArrayList<String>
+) : RecyclerView.Adapter<QuotesAdapter.ViewHolder>() {
+
+    var anime = anime
+    var character = character
+    lateinit var quote: ArrayList<String>
+    lateinit var imgurl: ArrayList<String>
+
 
     var titles = arrayOf("1", "2", "1")
     var details = arrayOf("2", "1", "3")
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuotesAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_saved_list, parent, false)
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_saved_list, parent, false)
         return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
 
-        return titles.size
+        return anime.size
     }
 
     override fun onBindViewHolder(holder: QuotesAdapter.ViewHolder, position: Int) {
-        holder.itemTitle.text = titles[position]
-        holder.itemDetail.text = titles[position]
+        holder.itemTitle.text = anime[position]
+        holder.itemDetail.text = character[position]
     }
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //var itemImage: ImageView
         var itemTitle: TextView
         var itemDetail: TextView
 
-        init{
+        init {
             //itemImage = itemView.findViewById(R.id.item_image)
             itemTitle = itemView.findViewById(R.id.item_title)
             itemDetail = itemView.findViewById(R.id.item_detail)
--- app/src/main/java/com/random/animequote/ui/RecyclerActivity.kt
+++ app/src/main/java/com/random/animequote/ui/RecyclerActivity.kt
@@ -2,16 +2,27 @@ package com.random.animequote.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.random.animequote.R
 import com.random.animequote.adapter.QuotesAdapter
+import com.random.animequote.db.DBHelper
+import com.random.animequote.model.Quote
 import kotlinx.android.synthetic.main.activity_recycler.*
 
 class RecyclerActivity : AppCompatActivity() {
 
     var layoutManager: RecyclerView.LayoutManager? = null
     var adapter: RecyclerView.Adapter<QuotesAdapter.ViewHolder>? = null
+
+    val db = DBHelper(this, null)
+    val quotes = ArrayList<Quote>()
+    val anime = ArrayList<String>()
+    val character = ArrayList<String>()
+    val quote = ArrayList<String>()
+    val imgurl = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +32,31 @@ class RecyclerActivity : AppCompatActivity() {
 
         savedList.layoutManager = layoutManager
 
-        adapter = QuotesAdapter()
+        storeQuotesInArray()
+        adapter = QuotesAdapter(this, anime, character, quote, imgurl)
+
         savedList.adapter = adapter
+    }
+
+    fun storeQuotesInArray() {
+        var cursor = db.getSavedQuotes()
+        if (cursor != null) {
+            if (cursor.count == 0) {
+                Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show()
+            } else {
+                while (cursor.moveToNext()) {
+                    var newQuote = Quote(
+                        cursor.getString(1), cursor.getString(2),
+                        cursor.getString(3), cursor.getString(4)
+                    )
+
+                    quotes.add(newQuote)
+                    anime.add(cursor.getString(1))
+                    character.add(cursor.getString(2))
+                    quote.add(cursor.getString(3))
+                    imgurl.add(cursor.getString(4))
+                }
+            }
+        }
     }
 }
